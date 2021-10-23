1b8d813fffe36e94ea87da0eaa86f809a16fb89c
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-23 16:58:50 -0600

    Make merchant index

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    merchants = Merchant.limit(merchant_limit).offset(merchant_offset)
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  private
+
+  def merchant_limit
+    params.fetch(:per_page, 20).to_i
+  end
+
+  def merchant_offset
+    page = params.fetch(:page, 1).to_i
+    page = 1 if page < 1
+    (page - 1) * merchant_limit
+  end
+end
