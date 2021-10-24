f3be86294cbf055ae26a10a56bd397c7e1c5d615
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-24 14:59:16 -0600

    Add tests and method for get merchant items

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -112,6 +112,49 @@ describe 'Items API' do
       get("/api/v1/merchants/#{@merchant_1.id}/items")
 
       expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items).to have_key(:data)
+      expect(items[:data]).to be_an(Array)
+
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq('item')
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
+    end
+
+    it 'does not include other merchants items' do
+      get("/api/v1/merchants/#{@merchant_1.id}/items")
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].first[:id]).to_not eq(@m2_items.first.id)
+    end
+
+    it 'throws a 404 for merchant not found' do
+      get("/api/v1/merchants/osudfgas/items")
+
+      expect(response.status).to be(404)
+
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:error]).to eq('Sorry, that merchant does not exist')
     end
   end
 end
