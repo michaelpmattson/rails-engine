a05ad5d304f2bc5ae2d13c7c633f4a5ac9917a09
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-24 18:47:24 -0600

    Add merchants find all

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_all_by_name(name)
+    where("LOWER(name) LIKE ?", "%#{name.downcase}%")
+      .order('LOWER(name)')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'items/find_all', to: 'items/find#index'
+      get 'items/find_all',     to: 'items/find#index'
       resources :items do
-        get 'merchant', to: 'items/merchant#show'
+        get 'merchant',         to: 'items/merchant#show'
       end
 
+      get 'merchants/find_all', to: 'merchants/find#index'
       resources :merchants, only: [:index, :show] do
         get 'items', to: 'merchant_items#index'
       end
