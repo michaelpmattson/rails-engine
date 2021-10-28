5194d9015da3f77f33c3473fed18a50e69ffbafb
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-28 11:50:00 -0600

    Move limits and offsets to ApplicationController

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.limit(item_limit).offset(item_offset)
+    items = Item.limit(result_limit).offset(page_offset)
     render json: ItemSerializer.new(items)
   end
 
@@ -33,16 +33,6 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   private
-
-  def item_limit
-    params.fetch(:per_page, 20).to_i
-  end
-
-  def item_offset
-    page = params.fetch(:page, 1).to_i
-    page = 1 if page < 1
-    (page - 1) * item_limit
-  end
 
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  private
+
+  def result_limit
+    params.fetch(:per_page, 20).to_i
+  end
+
+  def page_offset
+    page = params.fetch(:page, 1).to_i
+    page = 1 if page < 1
+    (page - 1) * result_limit
+  end
 end
